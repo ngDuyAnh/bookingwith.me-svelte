82c843ab895bb77afb4c80f1b802787a322c8df5
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-05-05 10:37:03 -0500

    Refresh button for schedule.

--- svelte/src/lib/page/protected/business-portal/page_lobby/page/Timetable/Timetable.svelte
+++ svelte/src/lib/page/protected/business-portal/page_lobby/page/Timetable/Timetable.svelte
@@ -6,7 +6,9 @@
     import {formatToDate, formatToTime} from "$lib/application/Formatter.js";
     import Calendar from '@event-calendar/core';
     import ResourceTimeGrid from '@event-calendar/resource-time-grid';
-    import EmployeeCard from "$lib/page/protected/business-portal/page_lobby/page/Timetable/EmployeeCard.svelte";
+    import ServicingTicketModal
+        from "$lib/page/protected/business-portal/page_lobby/page/Timetable/ServicingTicketModal/ServicingTicketModal.svelte";
+    import {Modal} from "flowbite-svelte";
 
     let open = false;
     let eventInfo = {};
@@ -68,32 +70,35 @@
         eventAllUpdated: function () {
             findECBody();
         }
-
-
     };
-
 
     let events = [];
     let resources = [];
-    let scheduleDone = false;
+    let loading = true;
 
+    function createEvents(employeeTimetableList) {
+        return employeeTimetableList.flatMap(employeeTable =>
+            employeeTable.servicingTicketList.map(servicingTicket => {
+                const title = servicingTicket.service.serviceName;
 
-    onMount(async () => {
+                return {
+                    // Event variables
+                    start: `${$now.format('YYYY-MM-DD')} ${servicingTicket.timePeriod.startTime}`,
+                    end: `${$now.format('YYYY-MM-DD')} ${servicingTicket.timePeriod.endTime}`,
+                    resourceId: employeeTable.employee.id,
+                    title: title,
 
-        function createEvents(employeeTimetableList) {
-            return employeeTimetableList.flatMap(employeeTable =>
-                employeeTable.servicingTicketList.map(servicingTicket => {
-                    const title = servicingTicket.service.serviceName;
+                    // Booking information
+                    employeeTimetable: employeeTable,
+                    servicingTicket: servicingTicket
+                };
+            })
+        );
+    }
 
-                    return {
-                        start: `${$now.format('YYYY-MM-DD')} ${servicingTicket.timePeriod.startTime}`,
-                        end: `${$now.format('YYYY-MM-DD')} ${servicingTicket.timePeriod.endTime}`,
-                        resourceId: employeeTable.employee.id,
-                        title: title,
-                    };
-                })
-            );
-        }
+    async function fetchSchedule()
+    {
+        loading = true;
 
         try {
             const response = await getSchedule($businessInfo.business.businessId, $now.format(formatToDate), $now.format(formatToTime));
@@ -110,29 +115,39 @@
             options.resources = resources;
             options.events = events;
 
-            if (options.events.length == 0)
+            if (options.events.length === 0)
             {
                 setTimeout(function() { findECBody(); }, 50);
             }
-
-            scheduleDone = true;
-
-
         } catch (error) {
             console.error('Failed to  fetch tasks', error);
             events = [];
             resources = [];
-            scheduleDone = false;
         }
+
+        loading = false;
+    }
+
+    onMount(async () => {
+        await fetchSchedule();
     });
+
+    $: console.log("eventInfo", eventInfo)
 
 </script>
 
 <div class="flex flex-col items-center justify-center p-1.5">
     Today - {$now.format("MMM D, YYYY")}
+    <button
+            class="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            on:click={fetchSchedule}
+            disabled={loading}
+    >
+        Refresh
+    </button>
 </div>
 
-{#if scheduleDone}
+{#if !loading}
     <div class="flex flex-col items-center justify-center w-4/5 h-4/5 mx-auto overflow-x-auto">
         <div class="flex h-full m-auto"
         >
@@ -141,7 +156,8 @@
     </div>
 {/if}
 
-<EmployeeCard
-        bind:open={open}
-        eventInfo={eventInfo}
-/>
+<div class="absolute top-0 left-0 right-0" style="z-index: 1006;">
+    <Modal bind:open={open} size="md" outsideclose>
+        {eventInfo.event.title}
+    </Modal>
+</div>
