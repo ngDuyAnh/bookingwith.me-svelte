e7ce9ec1992a1a747c1ce2dabf131a3bb89f1f87
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-05-07 11:03:55 -0500

    Working: Customer booking portal get page.

--- svelte/src/routes/customer-booking-portal/get/[bookingID]/+page.svelte
+++ svelte/src/routes/customer-booking-portal/get/[bookingID]/+page.svelte
@@ -3,28 +3,26 @@
     import {onMount} from "svelte";
     import {now} from "$lib/page/stores/now/now_dayjs_store.js";
     import {getCustomerBookingEstimate} from "$lib/api/api_server/customer-booking-portal/api.js";
-    import {formatToTime} from "$lib/application/Formatter.js";
+    import {formatToTime, formatToTimeAm} from "$lib/application/Formatter.js";
     import {Spinner} from "flowbite-svelte";
 
     export let data;
-
-    let today = $now.format('ddd, MMM D YYYY');
-    let isToday = false;
 
     let customerBooking = undefined;
     let queuePosition = -1;
     let servicingTimePeriod = undefined;
 
+    let relativeDate = 0;
     let bookingDateFormatted = "";
-    let bookingTime = "";
+    let bookingTimeFormatted = "";
 
-    async function fetchCustomerEstimate()
-    {
+    let estimateServicingStartTime = "";
+    let estimateServicingEndTime = "";
+
+    async function fetchCustomerEstimate() {
         // Get the estimate
         // Assume it is today
         const response = await getCustomerBookingEstimate(data.bookingID, $now.format(formatToTime));
-
-        console.log("customerBooking", response)
 
         // Extract the response
         customerBooking = response.customerBooking;
@@ -32,16 +30,24 @@
         servicingTimePeriod = response.servicingTimePeriod;
 
         // Display information
-        let bookingDate = dayjs(customerBooking.bookingDate);
-        isToday = bookingDate.isSame($now, 'day');
-        bookingDateFormatted = bookingDate.format('ddd, MMM D YYYY');
+        let bookingDate = dayjs(customerBooking.bookingDate).startOf("day");
+        relativeDate = bookingDate.diff($now.startOf("day"), "day");
 
-        bookingTime = customerBooking.bookingTime;
+        // Appointment date and time format for display
+        bookingDateFormatted = bookingDate.format('ddd, MMM D YYYY');
+        bookingTimeFormatted = dayjs(customerBooking.bookingTime, formatToTime).format(formatToTimeAm);
+
+        // Estimate servicing time
+        estimateServicingStartTime = dayjs(servicingTimePeriod.startTime, formatToTime).format(formatToTimeAm);
+        estimateServicingEndTime = dayjs(servicingTimePeriod.endTime, formatToTime).format(formatToTimeAm);
     }
 
     onMount(async () => {
         await fetchCustomerEstimate();
     });
+
+    // Automatic fetch
+    setInterval(async () => fetchCustomerEstimate(), 10000);
 
     let loading = true;
     $: if (customerBooking)
@@ -55,14 +61,14 @@
         <Spinner />
     </div>
 {:else}
-    <div class="flex flex-col h-screen bg-white text-gray-900">
+    <div class="flex flex-col text-gray-900">
 
         <div class="flex justify-between items-center p-6 shadow-md">
             <h1 class="text-3xl font-bold">
-                Welcome {customerBooking.customer.customerName},
+                Welcome!
             </h1>
             <h2 class="text-3xl font-light">
-                {today}
+                {$now.format('ddd, MMM D YYYY')}
             </h2>
         </div>
 
@@ -70,18 +76,36 @@
             <p class="bg-blue-100 text-blue-700 font-semibold rounded-lg p-4 shadow">
                 Your appointment is on
                 <span class="underline">{bookingDateFormatted}</span> at
-                <span class="underline">{bookingTime}</span>.
+                <span class="underline">{bookingTimeFormatted}</span>.
             </p>
 
-            {#if isToday}
-                <div class="mt-4 bg-green-100 text-green-800 rounded-lg p-4 shadow">
-                    Your position in the queue is {queuePosition}
-                </div>
+            {#if relativeDate === 0}
+                {#if customerBooking.bookingState === 0 || customerBooking.bookingState === 1}
+                    <div class="mt-4 bg-green-100 text-green-800 rounded-lg p-4 shadow">
+                        Your position in the queue is {queuePosition}
+                    </div>
+                    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
+                        Estimate start servicing time is from {estimateServicingStartTime} to {estimateServicingEndTime}
+                    </div>
+                {:else if customerBooking.bookingState === 2}
+                    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
+                        Estimate servicing end time is {estimateServicingEndTime}
+                    </div>
+                {:else}
+                    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
+                        Completed!
+                    </div>
+                {/if}
+            {:else if relativeDate > 0}
                 <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
-                    Estimate start servicing time is {servicingTimePeriod.startTime}
+                    Please check this link on your appointment day for real-time updates!
                 </div>
             {:else}
-                Check this link on your appointment day for real-time updates!
+                <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
+                    Under construction!
+                </div>
+
+                We (me, myself, and I) are still working on features after the appointment is done.
             {/if}
         </div>
 
