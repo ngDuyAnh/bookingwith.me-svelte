eef7d5c0f6f7ab7e29a19ad5fa0f228fa46e176b
Roba Geleta <geletaroba@yahoo.ca>
2024-05-26 01:44:01 -0500

    Styling for customer booking

--- svelte/src/lib/page/customer-booking-portal/get/page/Today/components/Completed/Completed.svelte
+++ svelte/src/lib/page/customer-booking-portal/get/page/Today/components/Completed/Completed.svelte
@@ -1,1 +1,5 @@
-<p>Completed</p>
+<div class="p-6 bg-white rounded-lg space-y-4">
+    <div class="bg-green-100 text-green-800 rounded-lg p-4 shadow-lg items-center">
+        Completed Appointment. Thank you!
+    </div>
+</div>
