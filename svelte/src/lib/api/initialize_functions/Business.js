97a64e525c80f05d4c59a2ecedc4195d2df1f698
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-05-28 22:55:03 -0500

    Add google review

--- svelte/src/lib/api/initialize_functions/Business.js
+++ svelte/src/lib/api/initialize_functions/Business.js
@@ -24,6 +24,7 @@ export function BusinessInformation()
         "websiteUrl": "",
         "businessType": "",
         "socialMediaLink": "",
+        "googleReviewLink": "",
         "timestamp": "",
         "active": true,
     };
