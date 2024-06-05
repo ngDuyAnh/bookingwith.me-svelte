8e01e3ac1ad663b38fce0b610dd54e69a8589adb
Roba Geleta <geletaroba@yahoo.ca>
2024-06-04 21:03:35 -0500

    update future customer booking portal - get page

--- svelte/src/lib/page/customer-booking-portal/get/page/Future/Future.svelte
+++ svelte/src/lib/page/customer-booking-portal/get/page/Future/Future.svelte
@@ -1,15 +1,12 @@
 <script>
     import {bookingEstimate} from "$lib/page/customer-booking-portal/get/stores/bookingEstimate.js";
-    import {now} from "$lib/page/stores/now/now_dayjs_store.js";
+    import {isTomorrow, now} from "$lib/page/stores/now/now_dayjs_store.js";
     import report_icon from "$lib/images/report_icon/message-report.png";
     import {formatPhoneNumber} from "$lib/application/FormatPhoneNumber.js";
-    import {isTomorrow} from "$lib/page/stores/now/now_dayjs_store.js";
     import {
-        handleCustomerBookingPortalEditCustomerBooking,
-        handleEditCustomerBooking
+        handleCustomerBookingPortalEditCustomerBooking
     } from "$lib/components/Modal/EditCustomerBooking/modalEditCustomerBooking.js";
     import {Button, Tooltip} from "flowbite-svelte";
-    import ModalEditCustomerBooking from "$lib/components/Modal/EditCustomerBooking/ModalEditCustomerBooking.svelte";
 
     //$:console.log($bookingEstimate);
 </script>
@@ -30,44 +27,67 @@
 
 
 </div>
-<div class="p-6 rounded-lg space-y-4">
-    <p class="bg-blue-100 text-blue-700 font-semibold rounded-lg p-4 shadow">
-        Your appointment is
-        {#if isTomorrow($bookingEstimate.customerBooking.bookingDate)}
+
+<div class="w-full h-full flex flex-col items-center justify-center  p-6 rounded-lg space-y-4">
+    <div class="flex items-center justify-center">
+        <div class="w-[max-content]">
+            <div
+                    class="relative text-xl rounded-t-lg font-bold flex bg-white text-blue-900 p-4 border-t-2 border-x-2"
+            >
+        <span>
+          Your appointment is
+            {#if isTomorrow($bookingEstimate.customerBooking.bookingDate)}
             <span class="underline">Tomorrow</span>
         {:else}
             on <span class="underline">{$bookingEstimate.bookingDateFormatted}</span>
 
         {/if}
-        at
+            at
         <span class="underline">{$bookingEstimate.bookingTimeFormatted}</span>.
-    </p>
+        </span>
 
+            </div>
 
-    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
-        Please visit this link on the day of your appointment for live updates!
+            <div class="bg-white text-green-800 p-4 flex flex-col justify-between border-x-2 text-sm">
+
+            </div>
+
+            <div
+                    class="relative bg-white flex flex-col  justify-between text-green-800 p-4 border-x-2 border-b-2 rounded-b-lg w-full"
+            >
+                <div class="flex flex-col justify-between w-full mt-auto">
+<span class="flex mt-1">
+                    Please visit this link on the day of your appointment for live updates!
+                </span>
+                    <span class="flex mt-1">
+                    You can edit your appointment online any time
+                    <span class="ml-1 font-semibold">before the day of your appointment</span>.
+                </span>
+
+                    <span class="flex mt-1">
+                    For further information, you can reach
+                    <span class="font-semibold mx-1">{$bookingEstimate.business.businessInfo.businessName}</span>
+                    at
+                    <span class="font-semibold ml-1">{formatPhoneNumber($bookingEstimate.business.businessInfo.businessPhoneNumber)}</span>.
+                </span>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
-        You can edit your appointment online any time
-        <span class="font-semibold">before the day of your appointment</span>.
+    <div>
+        <Button id="show-tooltip"
+                on:click={() => handleCustomerBookingPortalEditCustomerBooking($bookingEstimate.customerBooking)}
+                color="light" outline> Edit My Booking
+            <svg class="ml-1 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+            </svg>
+        </Button>
+        <Tooltip triggeredBy="#show-tooltip">Edit Booking</Tooltip>
     </div>
 
-    <div class="mt-2 bg-green-100 text-green-800 rounded-lg p-4 shadow">
-        For further information, you can reach
-        <span class="font-semibold">{$bookingEstimate.business.businessInfo.businessName}</span>
-        at
-        <span class="font-semibold">{formatPhoneNumber($bookingEstimate.business.businessInfo.businessPhoneNumber)}</span>.
-    </div>
 </div>
 
-<div>
-    <Button id="show-tooltip" on:click={() => handleCustomerBookingPortalEditCustomerBooking($bookingEstimate.customerBooking)} color="light" outline>
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-        </svg>
-    </Button>
-    <Tooltip triggeredBy="#show-tooltip">Edit Booking</Tooltip>
-</div>
+
