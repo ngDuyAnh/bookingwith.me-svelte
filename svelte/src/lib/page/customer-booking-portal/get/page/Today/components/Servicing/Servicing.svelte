313b54f038c3cfcbcf400c0c0cce5ee931d88d99
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-06-11 00:29:35 -0500

    Styling customer booking portal get servicing

--- svelte/src/lib/page/customer-booking-portal/get/page/Today/components/Servicing/Servicing.svelte
+++ svelte/src/lib/page/customer-booking-portal/get/page/Today/components/Servicing/Servicing.svelte
@@ -4,25 +4,20 @@
         from "$lib/page/customer-booking-portal/get/page/components/LiveIndicator/LiveIndicator.svelte";
 </script>
 
-<div class="h-full w-full flex flex-col items-center justify-center p-6 rounded-lg space-y-4">
-    <div class="flex items-center justify-center">
-        <div class="w-full max-w-4xl">
-            <!-- Appointment header -->
-            <div class="relative text-xl rounded-t-lg font-bold flex bg-white text-blue-900 p-4 border-t-2 border-x-2">
-                <div class="flex flex-row items-center justify-center w-full">
-                    <LiveIndicator/>
-                </div>
+<div class="h-full w-full flex flex-col items-center justify-center">
+    <!-- Appointment information box -->
+    <div class="bg-white max-w-4xl border-2 rounded-lg">
+        <!-- Appointment header -->
+        <div class="p-4 text-xl font-bold text-blue-900">
+            <div class="flex flex-row items-center justify-center w-full">
+                <LiveIndicator/>
             </div>
+        </div>
 
-            <!-- Appointment information -->
-            <div class="relative flex flex-col bg-white text-green-800 p-4 border-x-2 border-b-2 rounded-b-lg w-full">
-                <div class="flex justify-between w-full mt-auto">
-                    <span>Estimate servicing end time is</span>
-                    <div>
-                        <span class="animate-pulse text-sm font-semibold ml-1">{$bookingEstimate.estimateServicingEndTime}</span>
-                    </div>
-                </div>
-            </div>
+        <!-- Appointment information -->
+        <div class="p-4 text-green-800 text-sm">
+            <span>Estimate servicing end time is</span>
+            <span class="animate-pulse text-sm font-semibold">{$bookingEstimate.estimateServicingEndTime}</span>
         </div>
     </div>
 </div>
