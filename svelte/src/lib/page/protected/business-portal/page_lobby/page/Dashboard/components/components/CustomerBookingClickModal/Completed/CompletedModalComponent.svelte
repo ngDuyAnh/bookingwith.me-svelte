a309d86711fbe5aca624f9b3b94286387930984e
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-06-16 13:12:44 -0500

    None schedule management allow to move booking state

--- svelte/src/lib/page/protected/business-portal/page_lobby/page/Dashboard/components/components/CustomerBookingClickModal/Completed/CompletedModalComponent.svelte
+++ svelte/src/lib/page/protected/business-portal/page_lobby/page/Dashboard/components/components/CustomerBookingClickModal/Completed/CompletedModalComponent.svelte
@@ -14,14 +14,12 @@
     import {business} from "$lib/page/stores/business/business.js";
     import {checkAbleToSendReviewReminder} from "$lib/api/api_server/api_endpoints/lobby-portal/api.js";
     import {checkAbleToSendSmsReviewReminder} from "$lib/api/api_server/functions.js";
-    import {BusinessScheduleManagement} from "$lib/api/initialize_functions/Business.js";
 
     let ableToSendSmsReviewReminder = false;
 
     $: {
         // Check if sms review reminder can be sent
-        if ($customerBookingClickModal.customerBooking)
-        {
+        if ($customerBookingClickModal.customerBooking) {
             ableToSendSmsReviewReminder = false;
             checkAbleToSendReviewReminder($customerBookingClickModal.customerBooking)
                 .then(response => {
@@ -38,15 +36,12 @@
     // Retrieve customer booking list update function
     const submitCustomerBooking = getContext('submitCustomerBooking');
 
-    async function handleServicingClick()
-    {
+    async function handleServicingClick() {
         moveToServicing($now, $customerBookingClickModal.customerBooking, submitCustomerBooking);
     }
 
-    async function handleReviewSend()
-    {
-        if(ableToSendSmsReviewReminder && !$customerBookingClickModal.customerBooking.smsReviewReminderSent)
-        {
+    async function handleReviewSend() {
+        if (ableToSendSmsReviewReminder && !$customerBookingClickModal.customerBooking.smsReviewReminderSent) {
             sendSMSAskingForReview($business.businessInfo.businessName, $customerBookingClickModal.customerBooking);
             submitCustomerBooking($customerBookingClickModal.customerBooking);
         }
@@ -56,10 +51,18 @@
 <div>
     <p><strong>Customer name:</strong> {$customerBookingClickModal.customerBooking.customer.customerName}</p>
     <p><strong>Phone number:</strong> {$customerBookingClickModal.customerBooking.customer.phoneNumber}</p>
-    <p><strong>Booking time:</strong> {dayjs($customerBookingClickModal.customerBooking.bookingTime, formatToTime).format(formatToTimeAm)}</p>
-    <p><strong>Check-in time:</strong> {dayjs($customerBookingClickModal.customerBooking.checkinTime, formatToTime).format(formatToTimeAm)}</p>
-    <p><strong>Start time:</strong> {dayjs($customerBookingClickModal.customerBooking.servicingStartTime, formatToTime).format(formatToTimeAm)}</p>
-    <p><strong>End time:</strong> {dayjs($customerBookingClickModal.customerBooking.servicingEndTime, formatToTime).format(formatToTimeAm)}</p>
+    <p><strong>Booking
+        time:</strong> {dayjs($customerBookingClickModal.customerBooking.bookingTime, formatToTime).format(formatToTimeAm)}
+    </p>
+    <p><strong>Check-in
+        time:</strong> {dayjs($customerBookingClickModal.customerBooking.checkinTime, formatToTime).format(formatToTimeAm)}
+    </p>
+    <p><strong>Start
+        time:</strong> {dayjs($customerBookingClickModal.customerBooking.servicingStartTime, formatToTime).format(formatToTimeAm)}
+    </p>
+    <p><strong>End
+        time:</strong> {dayjs($customerBookingClickModal.customerBooking.servicingEndTime, formatToTime).format(formatToTimeAm)}
+    </p>
     {#if $customerBookingClickModal.customerBooking.message.length !== 0}
         <p class="break-words">
             <strong>Message:</strong> {$customerBookingClickModal.customerBooking.message}
@@ -90,9 +93,12 @@
                 outline
                 on:click={() => handleReviewSend()}
         >
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M11.209 3.816a1 1 0 0 0-1.966.368l.325 1.74a5.338 5.338 0 0 0-2.8 5.762l.276 1.473.055.296c.258 1.374-.228 2.262-.63 2.998-.285.52-.527.964-.437 1.449.11.586.22 1.173.75 1.074l12.7-2.377c.528-.1.418-.685.308-1.27-.103-.564-.636-1.123-1.195-1.711-.606-.636-1.243-1.306-1.404-2.051-.233-1.085-.275-1.387-.303-1.587-.009-.063-.016-.117-.028-.182a5.338 5.338 0 0 0-5.353-4.39l-.298-1.592Z"/>
-                <path fill-rule="evenodd" d="M6.539 4.278a1 1 0 0 1 .07 1.412c-1.115 1.23-1.705 2.605-1.83 4.26a1 1 0 0 1-1.995-.15c.16-2.099.929-3.893 2.342-5.453a1 1 0 0 1 1.413-.069Z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd"
+                      d="M6.539 4.278a1 1 0 0 1 .07 1.412c-1.115 1.23-1.705 2.605-1.83 4.26a1 1 0 0 1-1.995-.15c.16-2.099.929-3.893 2.342-5.453a1 1 0 0 1 1.413-.069Z"
+                      clip-rule="evenodd"/>
                 <path d="M8.95 19.7c.7.8 1.7 1.3 2.8 1.3 1.6 0 2.9-1.1 3.3-2.5l-6.1 1.2Z"/>
             </svg>
         </Button>
@@ -105,12 +111,9 @@
 
     <!--Right options-->
     <div class="ml-auto justify-end items-center space-x-2">
-        <!--No schedule management does not need to move the booking state-->
-        {#if $business.businessInfo.scheduleManagement !== BusinessScheduleManagement.NONE}
-            <span class="text-gray-700 font-bold">Move to:</span>
-            <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    on:click={handleServicingClick}>Servicing
-            </Button>
-        {/if}
+        <span class="text-gray-700 font-bold">Move to:</span>
+        <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                on:click={handleServicingClick}>Servicing
+        </Button>
     </div>
 </div>
