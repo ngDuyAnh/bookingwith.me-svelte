9ccaf7c48cfc807f559edae625290a5c4af298af
Roba Geleta <geletaroba@yahoo.ca>
2024-06-21 13:02:21 -0500

    implement sse for lobby

--- svelte/src/lib/page/protected/business-portal/page_lobby/BusinessPortalLobby.svelte
+++ svelte/src/lib/page/protected/business-portal/page_lobby/BusinessPortalLobby.svelte
@@ -11,15 +11,63 @@
     import SendReview from "$lib/page/protected/business-portal/page_lobby/page/SendReview/SendReview.svelte";
     // import { customerBookings, updateBookings} from "$lib/page/stores/testHook/booking.js";
     import {io} from 'socket.io-client';
+    import {onDestroy, onMount} from "svelte";
+    import {business} from "../../../stores/business/business.js";
 
     let tabs = ['Dashboard', 'Timetable', "List", "Send review", "Setting"];
     let selectedIndex = 0;
 
-    const socket = io();
 
-    socket.on('eventFromServer', (message)=>{
-        console.log(message);
-    })
+
+    let eventSource;
+
+    function connectSSE() {
+        if (eventSource) {
+            eventSource.close(); // Ensure the old connection is closed before creating a new one
+        }
+
+        const businessId = $business.businessInfo.businessID; // Make sure this reactive variable is accessed correctly
+        eventSource = new EventSource(`https://api-debug.bookingwith.me/sse/stream-sse/${businessId}`);
+
+        eventSource.addEventListener('business', function(event) {
+            const updatedBusiness = JSON.parse(event.data);
+            business.set(updatedBusiness);
+            console.log('Business updated:', updatedBusiness);
+        });
+
+        eventSource.onerror = function(error) {
+            console.error("Error. Trying to reconnect.");
+            eventSource.close();
+            setTimeout(connectSSE, 1000); // Reconnect logic
+        };
+    }
+
+    onMount(() => {
+        connectSSE();
+
+        window.addEventListener('beforeunload', () => {
+            if (eventSource) {
+                eventSource.close();
+            }
+        });
+    });
+
+    onDestroy(() => {
+        if (eventSource) {
+            eventSource.close(); // Close the connection when the component is destroyed
+        }
+    });
+
+    // HMR specific cleanup to prevent memory leaks and multiple connections
+    if (import.meta.hot) {
+        import.meta.hot.dispose(() => {
+            if (eventSource) {
+                eventSource.close();
+            }
+        });
+    }
+
+
 
 </script>
 
