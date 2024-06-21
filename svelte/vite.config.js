a3c2669ad1f57a6db8ab3fab99c081e268f6d4f1
Roba Geleta <geletaroba@yahoo.ca>
2024-06-21 13:04:55 -0500

    revert

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,20 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { Server } from 'socket.io';
-
 
 export default defineConfig({
-	plugins: [sveltekit(),
-		{
-			name: 'websocket-plugin',
-			configureServer(server) {
-				const io = new Server(server.httpServer);
-				io.on('connect', socket => {
-					socket.emit('eventFromServer', 'Hello World');
-				});
-			}
-		}
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
