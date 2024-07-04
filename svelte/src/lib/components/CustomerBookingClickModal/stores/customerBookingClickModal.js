a6032f0b777327ac7cc5e624022a6e5f04f2fcc8
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-07-03 19:23:10 -0500

    Update customerBookingClickModal.js

--- svelte/src/lib/components/CustomerBookingClickModal/stores/customerBookingClickModal.js
+++ svelte/src/lib/components/CustomerBookingClickModal/stores/customerBookingClickModal.js
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import {get, writable} from 'svelte/store';
 
 export const customerBookingClickModal = writable({
     open: false,
@@ -11,4 +11,35 @@ export function handleCustomerBookingClick(customerBooking)
         open: true,
         customerBooking: customerBooking
     });
+}
+
+export function handleCustomerBookingClickUpdate()
+{
+    const customerBookingClickModalValue = get(customerBookingClickModal);
+
+    // Find and reinitialize the customer booking for the modal
+    if (customerBookingClickModalValue.customerBooking)
+    {
+        const findID = customerBookingClickModalValue.customerBooking.id;
+        const foundCustomerBooking = findCustomerBookingById(findID);
+        if (foundCustomerBooking) {
+            customerBookingClickModal.update(current => {
+                return {
+                    ...current,
+                    customerBooking: foundCustomerBooking
+                };
+            });
+        }
+        // Customer booking not found
+        // Close the modal
+        else {
+            customerBookingClickModal.update(current => {
+                return {
+                    ...current,
+                    open: false
+                };
+            });
+            console.log('Customer booking not found for customer booking click modal.');
+        }
+    }
 }
