2db63e1a69fe82cbb7ba33efc1bae491f50b51e3
Roba Geleta <geletaroba@yahoo.ca>
2024-07-05 01:43:47 -0500

    update drag and drop implementation and sync with db

--- svelte/src/lib/page/stores/business/business.js
+++ svelte/src/lib/page/stores/business/business.js
@@ -5,9 +5,10 @@ export const business = writable(undefined);
 
 export function getEmployee(id)
 {
-    const businessValue = get(now);
+    const businessValue = get(business);
 
     if (businessValue && businessValue.employeeList) {
+        console.log("here");
         return businessValue.employeeList.find(employee => employee.id === id);
     }
     return null;
