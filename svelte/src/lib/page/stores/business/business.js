f51134ef16e55ebdb8fc060b834c1fd2a725fc9c
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-07-05 00:44:14 -0500

    $business get employee by id

--- svelte/src/lib/page/stores/business/business.js
+++ svelte/src/lib/page/stores/business/business.js
@@ -1,3 +1,14 @@
-import { writable } from 'svelte/store';
+import {get, writable} from 'svelte/store';
+import {now} from "$lib/page/stores/now/now_dayjs_store.js";
 
 export const business = writable(undefined);
+
+export function getEmployee(id)
+{
+    const businessValue = get(now);
+
+    if (businessValue && businessValue.employeeList) {
+        return businessValue.employeeList.find(employee => employee.id === id);
+    }
+    return null;
+}
