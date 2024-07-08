6a9e1d2d92f65f9f171a7f1b69a5c358bb440889
Roba Geleta <geletaroba@yahoo.ca>
2024-07-08 13:29:46 -0500

    type coearcion fix possibly

--- svelte/src/lib/page/protected/business-portal/page_lobby/page/Dashboard/components/components/CustomerBookingList/CustomerBookingList.svelte
+++ svelte/src/lib/page/protected/business-portal/page_lobby/page/Dashboard/components/components/CustomerBookingList/CustomerBookingList.svelte
@@ -26,16 +26,16 @@
             console.log("the customer booking", custBooking);
 
             if (custBooking) {
-                if (id == 0) {
+                if (id === 0) {
                     moveToAppointment(custBooking);
 
-                } else if (id == 1) {
+                } else if (id === 1) {
                     moveToLobby(custBooking);
 
-                } else if (id == 2) {
+                } else if (id === 2) {
                     moveToServicing(custBooking);
 
-                } else if (id == 3) {
+                } else if (id === 3) {
                     moveToCompleted(custBooking);
 
                 }
