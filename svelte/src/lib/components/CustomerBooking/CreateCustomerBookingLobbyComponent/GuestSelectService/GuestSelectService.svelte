5cd4483e663649776e10b600b9017d8fbf536781
Roba Geleta <geletaroba@yahoo.ca>
2024-07-09 17:00:19 -0500

    guest number increment, change trash icon

--- svelte/src/lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/GuestSelectService/GuestSelectService.svelte
+++ svelte/src/lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/GuestSelectService/GuestSelectService.svelte
@@ -10,7 +10,7 @@
 
 <!--List of service booking from the guest-->
 <div class="flex flex-col">
-    <span class="flex items-center w-full justify-center py-1 font-bold">Select Service(s) for Guest # {selectedIndividual} </span>
+    <span class="flex items-center w-full justify-center py-1 font-bold">Select Service(s) for Guest # {selectedIndividual+1} </span>
     {#each individualBooking.customerIndividualServiceBookingList as serviceBooking, index (index)}
         <SelectServiceBooking {serviceBooking}/>
         <button
