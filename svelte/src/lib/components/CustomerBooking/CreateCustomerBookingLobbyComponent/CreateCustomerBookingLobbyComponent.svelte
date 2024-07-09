2aa1a172224e8efc1ffd062d6fa347a2f0163401
Roba Geleta <geletaroba@yahoo.ca>
2024-07-09 14:31:24 -0500

    add column step indicator to new booking

--- svelte/src/lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/CreateCustomerBookingLobbyComponent.svelte
+++ svelte/src/lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/CreateCustomerBookingLobbyComponent.svelte
@@ -1,10 +1,8 @@
 <script>
     import {CustomerBooking, CustomerIndividualBooking} from "$lib/api/initialize_functions/CustomerBooking.js";
-    import SelectServiceBooking
-        from "$lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/GuestSelectService/components/SelectServiceBooking/SelectServiceBooking.svelte";
     import GuestSelectService
         from "$lib/components/CustomerBooking/CreateCustomerBookingLobbyComponent/GuestSelectService/GuestSelectService.svelte";
-    import {Tooltip} from "flowbite-svelte";
+    import {Avatar, Tooltip} from "flowbite-svelte";
 
     export let customerBooking = {
         ...CustomerBooking(),
@@ -22,11 +20,9 @@
     // Function to handle the deletion of an individual booking
     function deleteGuest(index) {
         // Always keep at least one guest
-        if (customerBooking.customerIndividualBookingList.length > 1)
-        {
+        if (customerBooking.customerIndividualBookingList.length > 1) {
             if (customerBooking.customerIndividualBookingList[index].customerIndividualServiceBookingList.length === 0 ||
-                confirm('Are you sure you want to delete this guest?'))
-            {
+                confirm('Are you sure you want to delete this guest?')) {
                 customerBooking.customerIndividualBookingList = [
                     ...customerBooking.customerIndividualBookingList.slice(0, index),
                     ...customerBooking.customerIndividualBookingList.slice(index + 1)
@@ -44,6 +40,13 @@
 
     <!--Guest column-->
     <div class="flex flex-col h-full">
+        <div class="relative flex flex-row flex-grow items-center justify-center mb-2">
+            <div class="absolute border-2 border-gray-200 h-1 w-full rounded-lg"></div>
+            <div class="flex flex-row bg-white z-10 space-x-1 px-1">
+                <Avatar size="xs" class="flex justify-center items-center">1</Avatar>
+                <span class="font-bold">Guest</span>
+            </div>
+        </div>
         <div class="w-[200px] h-full shadow overflow-y-auto">
             <!--Create a new guest-->
             <div class="flex justify-end">
@@ -51,8 +54,11 @@
                         on:click={createNewGuest}
                         class="new-guest select-none text-center cursor-pointer text-white px-2 py-2 flex flex-row justify-center items-center space-x-1"
                 >
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" fill="currentColor"
+                         viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                              d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z"
+                              clip-rule="evenodd"/>
                     </svg>
                     <span class="text-black text-sm">Add Guest</span>
                 </button>
@@ -60,21 +66,27 @@
 
             <ul class="flex-grow mt-4 space-y-2 px-2">
                 {#each customerBooking.customerIndividualBookingList as individualBooking, index (index)}
-                    <li id={index} class="flex justify-between items-center {selectedIndividualBookingIndex === index ? 'bg-blue-50 py-2 border-[1px] border-black':'bg-gray-100 py-1'} px-1 rounded-md shadow-sm ">
+                    <li id={index}
+                        class="flex justify-between items-center {selectedIndividualBookingIndex === index ? 'bg-blue-50 py-2 border-[1px] border-blue-700':'bg-gray-100 py-1'} px-1 rounded-md shadow-sm ">
                         <button
                                 on:click={() => {selectedIndividualBookingIndex = index}}
                                 class="flex-1 text-left cursor-pointer"
                         >
                         <span class="text-gray-950">
-                            Guest #{index + 1} (<span class="{individualBooking.customerIndividualServiceBookingList.length>0? '':'text-red-600'}">{individualBooking.customerIndividualServiceBookingList.length}</span>)
+                            Guest #{index + 1} (<span
+                                class="{individualBooking.customerIndividualServiceBookingList.length>0? '':'text-red-600'}">{individualBooking.customerIndividualServiceBookingList.length}</span>)
                         </span>
                         </button>
                         <button
                                 on:click={() => deleteGuest(index)}
                                 class="ml-4 text-red-500 cursor-pointer"
                         >
-                            <svg class="w-6 h-6 text-red-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+                            <svg class="w-6 h-6 text-red-800 dark:text-white" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                      d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
+                                      clip-rule="evenodd"/>
                             </svg>
                         </button>
                         <Tooltip type="light">Delete Guest</Tooltip>
@@ -88,7 +100,14 @@
 
     <!--Service selection for guest column-->
     <div class="flex-1 flex flex-col">
-        <ul class="flex-grow px-4 py-1 shadow w-full overflow-visible space-y-2">
+        <div class="relative flex flex-row flex-grow items-center justify-center mb-2">
+            <div class="absolute border-2 border-gray-200 h-1 w-full rounded-xl"></div>
+            <div class="flex flex-row bg-white z-10 space-x-1 px-1">
+                <Avatar size="xs" class="flex justify-center items-center">2</Avatar>
+                <span class="font-bold">Service</span>
+            </div>
+        </div>
+        <ul class="h-full px-4 py-1 shadow w-full overflow-visible space-y-2">
             <GuestSelectService
                     bind:selectedIndividual={selectedIndividualBookingIndex}
                     bind:individualBooking={
@@ -114,8 +133,8 @@
 </div>
 
 <style>
-    .new-guest{
-        background: rgb(0,212,255);
+    .new-guest {
+        background: rgb(0, 212, 255);
         background: linear-gradient(0deg, #d9d9d9 0%, rgba(255, 255, 255, 0) 100%);
         border-bottom-left-radius: 5px;
         color: #333;
