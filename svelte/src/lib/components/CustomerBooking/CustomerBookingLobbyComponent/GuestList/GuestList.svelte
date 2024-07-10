ff5a8638a628270759a61194e62a601af8ed103d
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-07-10 00:04:34 -0500

    Remove the delete button if there is only 1 guest.

--- svelte/src/lib/components/CustomerBooking/CustomerBookingLobbyComponent/GuestList/GuestList.svelte
+++ svelte/src/lib/components/CustomerBooking/CustomerBookingLobbyComponent/GuestList/GuestList.svelte
@@ -31,25 +31,26 @@
                         on:click={() => {selectedIndividualBookingIndex = index}}
                         class="flex-1 text-left cursor-pointer"
                 >
-                    {#key customerBooking.customerIndividualBookingList[index].customerIndividualServiceBookingList}
                         <span class="text-gray-950">
                             Guest #{index + 1} (<span
                                 class="{individualBooking.customerIndividualServiceBookingList.length>0? '':'text-red-600'}">{individualBooking.customerIndividualServiceBookingList.length}</span>)
                         </span>
-                    {/key}
                 </button>
-                <button
-                        on:click={() => deleteGuest(index)}
-                        class="ml-4 text-red-500 cursor-pointer"
-                >
-                    <svg class="w-4 h-4 text-red-800 dark:text-white" aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                         viewBox="0 0 448 512">
-                        <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                    </svg>
-                </button>
-                <Tooltip type="light">Delete Guest</Tooltip>
+
+                {#if customerBooking.customerIndividualBookingList.length > 1}
+                    <button
+                            on:click={() => deleteGuest(index)}
+                            class="ml-4 text-red-500 cursor-pointer"
+                    >
+                        <svg class="w-4 h-4 text-red-800 dark:text-white" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                             viewBox="0 0 448 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                        </svg>
+                    </button>
+                    <Tooltip type="light">Delete Guest</Tooltip>
+                {/if}
             </li>
     {/each}
 </ul>
