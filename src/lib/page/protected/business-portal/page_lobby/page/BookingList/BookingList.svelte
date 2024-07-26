ac8cd27caf203f4138d42e1257bafdb76f3884fb
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-07-26 12:25:04 -0500

    Lobby booking list implemented search

--- src/lib/page/protected/business-portal/page_lobby/page/BookingList/BookingList.svelte
+++ src/lib/page/protected/business-portal/page_lobby/page/BookingList/BookingList.svelte
@@ -15,7 +15,9 @@
     import BookingListCustomerBookingClickModal
         from "$lib/page/protected/business-portal/page_lobby/page/BookingList/components/BookingListCustomerBookingClickModal/BookingListCustomerBookingClickModal.svelte";
     import {ChevronLeftOutline, ChevronRightOutline} from "flowbite-svelte-icons";
-    import {Button} from "flowbite-svelte";
+    import {Button, Search} from "flowbite-svelte";
+    import {normalizeSearchInput} from "$lib/application/NormalizeSearchInput.js";
+    import {shortCustomerBookingID} from "$lib/api/utilitiy_functions/CustomerBooking.js";
 
     let tomorrow = $now.startOf('day').add(1, 'day');
 
@@ -53,6 +55,29 @@
 
     function selectYesterday() {
         selectedDate = dayjs(selectedDate).subtract(1, "day").format(formatToDate);
+    }
+
+    $: if (searchValue.length >= 0 || $bookingList.customerBookingList) {
+        handleSearchFilter();
+    }
+
+    let searchValue = '';
+    let filteredCustomerBookingList = $bookingList.customerBookingList;
+
+    function handleSearchFilter() {
+        const normalizedSearchValue = normalizeSearchInput(searchValue);
+
+        // Assume $customerBookingQueueList is structured like: [ [], [], [], [] ]
+        // where each sub-array represents bookings in different states: 0->appointment, 1->lobby, 2->servicing, 3->completed
+        if (searchValue.length === 0) {
+            filteredCustomerBookingList = $bookingList.customerBookingList;
+        } else {
+            filteredCustomerBookingList = $bookingList.customerBookingList.filter(booking =>
+                    normalizeSearchInput(booking.customer.customerName).includes(normalizedSearchValue) ||
+                    normalizeSearchInput(shortCustomerBookingID(booking.id)).includes(normalizedSearchValue) ||
+                    normalizeSearchInput(booking.customer.phoneNumber.toString()).includes(normalizedSearchValue)
+            );
+        }
     }
 
     $: console.log("$bookingList.customerBookingList", $bookingList.customerBookingList);
@@ -100,12 +125,18 @@
             </Button>
         </div>
     </div>
+
+    <form class="flex max-w-xs items-center">
+        <Search bind:value={searchValue} size="md" class="rounded-none rounded-l-lg py-2.5"
+                placeholder="Search Booking Info" maxlength="20">
+        </Search>
+    </form>
 </div>
 
 <!-- Appointment list -->
 <div class="overflow-y-auto">
     <ul>
-        {#each $bookingList.customerBookingList as customerBooking (customerBooking.id)}
+        {#each filteredCustomerBookingList as customerBooking (customerBooking.id)}
             <li class="border-b last:border-b-0">
                 <button
                         class="mb-4 p-4 bg-white rounded shadow select-none w-full text-left
