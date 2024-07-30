c256a7de8c79efa6b4b7bf3fc2060382850d0fcc
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2024-07-30 17:45:28 -0500

    Bring back the console removal.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import path from "path"
-import removeConsole from "vite-plugin-svelte-console-remover";
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
+	// https://github.com/vitejs/vite/discussions/7920
+	esbuild: {
+		pure: ['console.log', "console.warn"]
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
