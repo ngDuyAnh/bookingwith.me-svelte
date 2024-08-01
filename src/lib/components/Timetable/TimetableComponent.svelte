aa9de047e74ca1c98e1e723c161ed1c0c1ac0906
Roba Geleta <geletaroba@yahoo.ca>
2024-08-01 00:21:14 -0500

    fix tooltips not showing since custom tooltip and flowbite tooltip both use same name tooltip class.

--- src/lib/components/Timetable/TimetableComponent.svelte
+++ src/lib/components/Timetable/TimetableComponent.svelte
@@ -151,7 +151,7 @@
                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-[15px] h-[15px] text-red-500" fill="currentColor" viewBox="0 0 640 512">
                                                  <path fill="currentColor" d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3zM471 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
                                                </svg>
-                                               <span class="tooltip">No employee assigned</span>
+                                               <span class="custom-tooltip">No employee assigned</span>
                                              </div>`;
                 // console.log("mounted");
                 let extendedProps = info.event.extendedProps;
@@ -1006,6 +1006,8 @@
     function selectYesterday() {
         selectedDate = dayjs(selectedDate).subtract(1, "day").format(formatToDate);
     }
+
+    $:console.log("$timetableComponent",$timetableComponent);
 </script>
 
 <div
@@ -1222,7 +1224,7 @@
         display: block;
     }
 
-    :global(.tooltip) {
+    :global(.custom-tooltip) {
         visibility: hidden;
         background-color: #4f4d4d;
         width: auto; /* Adjust width dynamically */
@@ -1246,7 +1248,7 @@
     }
 
 
-    :global(.tooltip-container:hover .tooltip) {
+    :global(.tooltip-container:hover .custom-tooltip) {
         visibility: visible;
         opacity: 1;
     }
