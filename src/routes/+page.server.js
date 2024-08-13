cdf715b253fa5e9ccdb580e5f81c5f6c26668b19
Roba Geleta <geletaroba@yahoo.ca>
2024-08-12 23:26:27 -0500

    remove log

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,17 +2,14 @@ import {login} from "$lib/api/api_server/api_endpoints/user-portal/api.js";
 import {redirect} from "@sveltejs/kit";
 
 export async function load({ cookies }) {
-    console.log("loading landing page");
     const authCookie = await cookies.get('auth');
     const auth = authCookie ? JSON.parse(authCookie) : null;
 
     // Get the user profile from the api backend
     let user = null;
-    console.log("~~~AUth is",auth);
     let valid=false;
     if (auth)
     {
-        console.log("auth is",auth);
         try{
             user = await login(auth.email);
             valid=true;
@@ -20,9 +17,7 @@ export async function load({ cookies }) {
         catch{
             valid=false;
         }
-        console.log("user", user);
     }
-
 
     // Return data
     return {
